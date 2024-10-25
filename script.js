64b431d401a5a11441ccd3fae1ab1e9e33f45324
Prince Kumar <91559607+prince7858@users.noreply.github.com>
2024-10-25 23:47:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	if (s.length === 0) {
+        return s;
+    }
+    let spaceIndex = s.indexOf(' ');
+    if (spaceIndex === -1) {
+        return s;
+    }
+    return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
